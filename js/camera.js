a13b1f3358090fa1c1b1d183860b1bde31ae3c9b
Rosangela <96451367+Rommendes@users.noreply.github.com>
2025-02-04 17:25:11 -0300

    up

--- js/camera.js
+++ js/camera.js
@@ -5,7 +5,9 @@ const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
 const canvas = document.querySelector("[data-video-canvas]");
 const mensagem = document.querySelector("[data-mensagem]");
 const novaFoto = document.querySelector("[data-tirar-nova-foto]");
+const bontaoEnviarFoto = document.querySelector("[data-enviar] ");
 
+let imagemURL = '';
 
 botaoIniciarCamera.addEventListener("click", tirarFoto)
     async function tirarFoto(){
@@ -17,20 +19,40 @@ botaoIniciarCamera.addEventListener("click", tirarFoto)
 
     video.srcObject = iniciarVideo;
 
-    let imagemURL = "";
+    
 }
-botaoTirarFoto.addEventListener("click", function(){
-    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.heigth)
+//botaoTirarFoto.addEventListener("click", function(){
+//    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.heigth);
+//
+//    imagemURL = canvas.toDataURL('image/jpeg');
 
-    imagemURL = canvas.toDataURL("image/jpeg");
+ //   campoCamera.style.display = "none";
+ //   mensagem.style.display = "block"
+
+//   
+//});
+botaoTirarFoto.addEventListener('click', function () {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    imagemURL = canvas.toDataURL('image/jpeg');
 
     campoCamera.style.display = "none";
-    mensagem.style.display = "block"
-})
-
+    mensagem.style.display = "block";
+});
 
 novaFoto.addEventListener("click", () => {
     mensagem.style.display = "none";
     botaoIniciarCamera.style.display = "block";
-    tirarFoto;
+    tirarFoto();
+})
+
+bontaoEnviarFoto.addEventListener("click", ()=> {
+    const receberDadosExistentes = localStorage.getItem("cadastro");
+    const converteRetorno = JSON.parse(receberDadosExistentes);
+
+    converteRetorno.imagem =imagemURL;
+
+    localStorage.setItem('cadastro', JSON.stringify(converteRetorno));
+
+    window.location.href = "./abrir-conta-form-3.html";
 })
