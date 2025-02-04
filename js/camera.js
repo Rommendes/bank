52862b56b2f871ad58505edbcbf9cb4513809c3a
Rosangela <96451367+Rommendes@users.noreply.github.com>
2025-02-04 13:49:12 -0300

    Tirar foto

--- js/camera.js
+++ js/camera.js
@@ -1,15 +1,36 @@
 const botaoIniciarCamera = document.querySelector("[data-video-botao]");
-
 const campoCamera = document.querySelector("[data-camera]");
-
 const video = document.querySelector("[data-video]");
+const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
+const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]");
+const novaFoto = document.querySelector("[data-tirar-nova-foto]");
 
-botaoIniciarCamera.addEventListener("click", async function () {
-    const iniciarVideo = await navigator.mediaDevices
-    .getUserMedia({video: true, audio: false});
 
+botaoIniciarCamera.addEventListener("click", tirarFoto)
+    async function tirarFoto(){
+        const iniciarVideo = await navigator.mediaDevices
+        .getUserMedia({video: true, audio: false})
+    
     botaoIniciarCamera.style.display ="none";
     campoCamera.style.display ="block";
 
     video.srcObject = iniciarVideo;
+
+    let imagemURL = "";
+}
+botaoTirarFoto.addEventListener("click", function(){
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.heigth)
+
+    imagemURL = canvas.toDataURL("image/jpeg");
+
+    campoCamera.style.display = "none";
+    mensagem.style.display = "block"
+})
+
+
+novaFoto.addEventListener("click", () => {
+    mensagem.style.display = "none";
+    botaoIniciarCamera.style.display = "block";
+    tirarFoto;
 })
